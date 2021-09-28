5bb2893148566650933d8f12ed04806af0607b68
Stephane Bisson <sbisson@wikimedia.org>
2021-09-28 10:47:35 -0400

    Basic element-level selection

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -12,9 +12,6 @@
     export default {
       name: 'Article',
       props: ['article'],
-      data: () => {
-        return { begin: true, r: { b: { n: null, o: 0 }, e: { n: null, o: 0 } }, t: '' }
-      },
       computed: mapGetters(['currentArticle']),
       methods: {
         ...mapActions(['fetchArticle', 'setText']),
@@ -22,62 +19,33 @@
           this.$router.go(-1)
         },
         onConfirm: function() {
-          if (this.t) {
-            this.setText( this.t )
-            this.$router.push({name: 'Story'})
+          const selected = document.querySelector('.selected-text')
+          if (selected) {
+            const text = selected.innerText
+            if (text) {
+              this.setText( selected.innerText )
+              this.$router.push( { name: 'Story' } )
+            }
           }
         },
         onClick: function(e) {
           e.preventDefault()
-          const r = document.caretRangeFromPoint(e.clientX, e.clientY)
-          const selection = document.getSelection()
-          const range = selection.getRangeAt(0)
-          console.log('compare point', range.comparePoint(r.startContainer, r.startOffset))
-          // console.log('START_TO_START', range.compareBoundaryPoints(Range.START_TO_START, r))
-          // console.log('START_TO_END', range.compareBoundaryPoints(Range.START_TO_END, r))
-          // console.log('END_TO_END', range.compareBoundaryPoints(Range.END_TO_END, r))
-          // console.log('END_TO_START', range.compareBoundaryPoints(Range.END_TO_START, r))
-          // if (range.compareBoundaryPoints(Range.START_TO_START, r) === 1) {
-          //   console.log('before')
-          // } else if (range.compareBoundaryPoints(Range.END_TO_END, r) === -1) {
-          //   console.log('after')
-          // } else {
-          //   console.log('inside')
-          // }
-          if ( this.r.b.n && this.r.e.n) {
-            return;
+          const selected = document.querySelector('.selected-text')
+          if (selected) {
+            selected.classList.remove('selected-text')
           }
-          if (this.begin) {
-            this.r.b.n = r.startContainer
-            this.r.b.o = r.startOffset
-            this.begin = false
-            if (this.r.e.n === null) {
-              this.r.e.n = r.startContainer
-              this.r.e.o = r.startOffset + 1
-            }
-          } else {
-            this.r.e.n = r.startContainer
-            this.r.e.o = r.startOffset
-            this.begin = true
-          }
-          selection.setBaseAndExtent(this.r.b.n, this.r.b.o, this.r.e.n, this.r.e.o)
-          this.t = selection.toString()
+          e.target.classList.add('selected-text')
         }
       },
       created: function () {
         this.fetchArticle(this.article)
       },
-      // updated: () => {
-      //   document.addEventListener('selectionchange', () => {
-      //     console.log(document.getSelection());
-      //   });
-      // }
     }
 </script>
 <style>
     .toolbar {
         width: 100vw;
-        /*height: 20px;*/
+        height: 45px;
         display: flex;
         flex-direction: row;
         align-content: stretch;
@@ -94,9 +62,14 @@
     .article {
         overflow: scroll;
         padding: 32px 16px 0 16px;
+        cursor: pointer;
+        position: absolute;
+        top: 45px;
+        bottom: 0;
+        right: 0;
+        left: 0;
     }
     .article p {
-        /*cursor: pointer;*/
         position: inherit;
     }
     .article section {
@@ -106,5 +79,8 @@
         width: 100%!important;
         max-width: 100%!important;
         margin: 0;
+    }
+    .selected-text {
+        background-color: yellow;
     }
 </style>
