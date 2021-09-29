7895f890c7c0e1bc88198f4f053a31e6b9f8eff9
Stephane Bisson <sbisson@wikimedia.org>
2021-09-29 14:43:54 -0400

    Use standard text selection

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -4,62 +4,62 @@
             <div @click="onBack">Back</div>
             <div @click="onConfirm">Confirm</div>
         </div>
-        <div class="article" @click="onClick" v-html="currentArticle"></div>
+        <div class="article" @mouseup="onMouseUp" v-html="currentArticle"></div>
+        <div :style="selectionToolbarStyle">
+            <div @click="onUseText">Use this text</div>
+            <div @click="onDismiss">Dismiss</div>
+        </div>
     </div>
 </template>
 <script>
-    import { mapGetters, mapActions } from 'vuex'
-    export default {
-      name: 'Article',
-      props: ['article'],
-      computed: mapGetters(['currentArticle']),
-      methods: {
-        ...mapActions(['fetchArticle', 'setText', 'setImg']),
-        onBack: function() {
-          this.$router.go(-1)
-        },
-        onConfirm: function() {
-          const selectedText = document.querySelector('.selected-text')
-          const text = selectedText && selectedText.innerText
+  import {mapGetters, mapActions} from 'vuex'
 
-          const selectedImage = document.querySelector('.selected-image')
-          const imageSrc = selectedImage && selectedImage.src
-
-            if (text && imageSrc) {
-              this.setText(text)
-              this.setImg(imageSrc)
-              this.$router.push( { name: 'Story' } )
-            }
+  export default {
+    name: 'Article',
+    props: ['article'],
+    data: () => {
+      return {
+        selectionToolbarStyle: {
+          display: 'none',
+          position: 'absolute',
+          bottom: 0
         },
-        onClick: function(e) {
-          e.preventDefault()
-          if (e.target.tagName === 'IMG') {
-            const selectedImage = document.querySelector('.selected-image')
-            if (selectedImage) {
-              selectedImage.classList.remove('selected-image')
-              if (selectedImage !== e.target) {
-                e.target.classList.add('selected-image')
-              }
-            } else {
-              e.target.classList.add('selected-image')
-            }
-          } else {
-            const selectedText = document.querySelector( '.selected-text' )
-            if ( selectedText ) {
-              selectedText.classList.remove( 'selected-text' )
-              if (selectedText !== e.target) {
-                e.target.classList.add('selected-text')
-              }
-            } else {
-              e.target.classList.add( 'selected-text' )
-            }
-          }
+        selectedText: null
+      }
+    },
+    computed: mapGetters( ['currentArticle'] ),
+    methods: {
+      ...mapActions( ['fetchArticle', 'setText', 'setImg'] ),
+      onBack: function () {
+        this.$router.go( -1 )
+      },
+      onConfirm: function () {
+      },
+      onMouseUp: function () {
+        const s = document.getSelection()
+        const r = s && s.getRangeAt( 0 )
+        if ( r && !r.collapsed ) {
+          const rect = r.getBoundingClientRect()
+          this.selectedText = s.toString()
+          this.selectionToolbarStyle.display = 'block'
+          this.selectionToolbarStyle.top = ( rect.top - rect.height ) + 'px';
+          this.selectionToolbarStyle.left = rect.left + 'px';
+        } else {
+          this.selectionToolbarStyle.display = 'none'
         }
       },
-      created: function () {
-        this.fetchArticle(this.article)
+      onUseText: function () {
+        this.setText( this.selectedText )
+        this.$router.push( { name: 'Story' } )
       },
-    }
+      onDismiss: function () {
+        this.selectionToolbarStyle.display = 'none'
+      },
+    },
+    created: function () {
+      this.fetchArticle( this.article )
+    },
+  }
 </script>
 <style>
     .toolbar {
@@ -71,6 +71,7 @@
         align-items: center;
         background-color: dimgray;
     }
+
     .toolbar > div {
         flex: auto;
         margin: 10px;
@@ -78,31 +79,28 @@
         font-weight: bold;
         cursor: pointer;
     }
+
     .article {
         overflow: scroll;
         padding: 32px 16px 0 16px;
-        cursor: pointer;
         position: absolute;
         top: 45px;
         bottom: 0;
         right: 0;
         left: 0;
     }
+
     .article p {
         position: inherit;
     }
+
     .article section {
         display: block !important;
     }
+
     .article figure {
-        width: 100%!important;
-        max-width: 100%!important;
+        width: 100% !important;
+        max-width: 100% !important;
         margin: 0;
-    }
-    .selected-text {
-        background-color: yellow;
-    }
-    .selected-image {
-        outline: dashed yellow 10px;
     }
 </style>
