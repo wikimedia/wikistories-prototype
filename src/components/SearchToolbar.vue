2f52e70f2eef5c4f36378a80603287c5d86a4137
Stephane Bisson <stephane.c.bisson@gmail.com>
2021-10-03 13:54:04 -0400

    Search toolbar (#12)
    * Search toolbar
    
    * Add icons and i18n
    
    Co-authored-by: Stephane Bisson <sbisson@wikimedia.org>

---
+++ src/components/SearchToolbar.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="search-toolbar">
+        <router-link to="/searchcommons" class="commons logo">{{ $i18n('wikimedia-commons') }}</router-link>
+        <router-link to="/searchwikipedia" class="wikipedia logo">{{ $i18n('wikipedia') }}</router-link>
+    </div>
+</template>
+<script>
+    export default {
+      name: 'SearchToolbar'
+    }
+</script>
+<style scoped>
+    .search-toolbar {
+        border-top: solid black 1px;
+        padding: 5px;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+    }
+    .commons {
+        background-image: url(../images/commons.png);
+    }
+    .wikipedia {
+        background-image: url(../images/wikipedia.png);
+    }
+    .logo {
+        background-repeat: no-repeat;
+        background-position: top;
+        background-size: 30px;
+        padding-top: 30px;
+        text-decoration: none;
+        color: black;
+    }
+</style>
--- src/views/Story.vue
+++ src/views/Story.vue
@@ -2,17 +2,20 @@
 <div class="view story">
     <CurrentFrame />
     <Frames />
+    <SearchToolbar />
 </div>
 </template>
 <script>
 import CurrentFrame from '@components/CurrentFrame.vue'
 import Frames from '@components/Frames.vue'
+import SearchToolbar from '@components/SearchToolbar.vue'
 
 export default {
   name: 'Story',
   components: {
         CurrentFrame,
-        Frames
+        Frames,
+        SearchToolbar
   }
 }
 </script>
