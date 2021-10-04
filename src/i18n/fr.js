5e373d780fbaa3358c7dfb0258221e437068d4df
Stephane Bisson <sbisson@wikimedia.org>
2021-10-04 13:14:42 -0400

    Add french messages

--- src/i18n/fr.js
+++ src/i18n/fr.js
@@ -1,4 +1,9 @@
 export default {
   'welcome': 'Bienvenue sur les Wikistoires',
   'btn-create': 'Creer une histoire',
+  'search-box': 'Recherche dans Wikipédia',
+  'search-media': 'Recherche de Média',
+  'search-media-info': '$1 {{PLURAL:$1|selectionné|selectionnés}}',
+  'wikimedia-commons': 'Commons',
+  'wikipedia': 'Wikipédia',
 }
