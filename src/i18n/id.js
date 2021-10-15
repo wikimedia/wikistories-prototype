31039ca29f902e344cdf4f2522ee5a4934cc6c2b
Stephane Bisson <sbisson@wikimedia.org>
2021-10-15 09:31:34 -0400

    Localization to id

---
+++ src/i18n/id.js
@@ -0,0 +1,16 @@
+export default {
+  'welcome': 'Welcome to Wikistories',
+  'btn-create': 'Buat cerita',
+  'btn-restart-story': 'mulai dari awal',
+  'search-box': 'Cari di wikipedia',
+  'search-media': 'Cari media',
+  'search-media-info': '$1 dipilih',
+  'wikimedia-commons': 'Commons',
+  'wikipedia': 'Wikipedia',
+  'btn-publish': 'Terbitkan',
+  'btn-publish-wikistory': 'Publikasikan Wikistory',
+  'publish-date': 'Tanggal',
+  'publish-tags': 'Tags',
+  'publish-language': 'Bahasa',
+  'tags-not-set': 'Belum disetel'
+}
--- src/i18n/index.js
+++ src/i18n/index.js
@@ -2,10 +2,11 @@ import Vue from 'vue'
 import i18n from 'vue-banana-i18n'
 import en from './en'
 import fr from './fr'
+import id from './id'
 
 import { lang } from '@utils/lang'
 
 Vue.use(i18n, {
   locale: lang,
-  messages: { en, fr }
+  messages: { en, fr, id }
 })
