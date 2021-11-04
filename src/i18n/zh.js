b456c9c16e1b0e47c9c4cedb767ef29deb96846d
Huei Tan <hueitan@users.noreply.github.com>
2021-11-04 15:52:17 +0100

    Create zh.js

---
+++ src/i18n/zh.js
@@ -0,0 +1,16 @@
+export default {
+  'welcome': '歡迎光臨 Wikistories',
+  'btn-create': '創建故事',
+  'btn-restart-story': '重來',
+  'search-box': '搜尋維基百科',
+  'search-media': '搜尋媒體',
+  'search-media-info': '$1 選擇',
+  'wikimedia-commons': 'Commons',
+  'wikipedia': '維基百科',
+  'btn-publish': '發佈',
+  'btn-publish-wikistory': '發佈 Wikistory',
+  'publish-date': '日期',
+  'publish-tags': '標簽',
+  'publish-language': '語言',
+  'tags-not-set': '尚未設定'
+}
