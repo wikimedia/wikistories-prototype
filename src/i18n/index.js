0bf8da8564b0e3fe6007dcf3d87a27ca8ccabe64
Huei Tan <hueitan@users.noreply.github.com>
2021-11-04 16:37:51 +0100

    Add localization file for zh language (#26)
    * Create zh.js
    
    * add zh localication
    
    * Update commons text
    
    * Update zh.js
    
    * add missing translation

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -3,10 +3,11 @@ import i18n from 'vue-banana-i18n'
 import en from './en'
 import fr from './fr'
 import id from './id'
+import zh from './zh'
 
 import { lang } from '@utils/lang'
 
 Vue.use(i18n, {
   locale: lang,
-  messages: { en, fr, id }
+  messages: { en, fr, id, zh }
 })
---
+++ src/i18n/zh.js
@@ -0,0 +1,20 @@
+export default {
+  'welcome': '歡迎光臨 Wikistories',
+  'btn-create': '創建故事',
+  'btn-restart-story': '重新開始',
+  'search-box': '搜尋維基百科',
+  'search-media': '搜尋媒體',
+  'search-media-info': '$1 選擇',
+  'wikimedia-commons': '維基共享資源',
+  'wikipedia': '維基百科',
+  'btn-publish': '發佈',
+  'btn-publish-wikistory': '發佈 Wikistory',
+  'btn-highlight': 'Highlight',
+  'btn-clear': '移除',
+  'btn-select-image': '選擇圖片',
+  'btn-next': '下一步',
+  'publish-date': '日期',
+  'publish-tags': '標簽',
+  'publish-language': '語言',
+  'tags-not-set': '尚未設定'
+}
