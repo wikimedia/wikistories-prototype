b2ef7ec55800565b94809445983c534bb00fad38
Stephane Bisson <sbisson@wikimedia.org>
2021-11-05 10:46:38 -0400

    Fix article view

--- src/App.vue
+++ src/App.vue
@@ -46,5 +46,6 @@ body {
   height: 100vh; /* Fallback for browsers that do not support Custom Properties */
   height: calc(var(--vh, 1vh) * 100);
   overflow: hidden;
+  position: relative;
 }
 </style>
